4d3f4b2cad8481e8162a350ec533a5158ebd322c
Luis <lavp99@hotmail.com>
2022-10-28 21:35:52 +0200

    cais, pero despues de hablar con profes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+
+const misCartas = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const misPalos = ["♦", "♥", "♠", "♣"];
+const misColores = ["diamond", "heart", "spade", "club"];
+
+function aleatorio(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const miNumero = document.getElementById("numero");
+  miNumero.innerHTML = misCartas[aleatorio(0, 12)];
+
+  const miPaloSup = document.getElementById("paloSup");
+  let nPalo = aleatorio(0, 3);
+  miPaloSup.innerHTML = `<h1> ${misPalos[nPalo]} </h1>`;
+
+  const miPaloInf = document.getElementById("paloInf");
+  miPaloInf.innerHTML = `${misPalos[nPalo]}`;
+
+  const micarta = document.getElementById("carta");
+  micarta.className = "mx-auto card " + misColores[nPalo];
 };
